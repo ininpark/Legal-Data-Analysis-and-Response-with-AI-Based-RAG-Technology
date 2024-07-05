90f699d56119fbfc71e4c272f4e6ca708320fa33
inhye <139088313+ininpark@users.noreply.github.com>
2024-07-06 01:42:38 +0900

    Update generate_law_pdf.py

--- generate_law_pdf.py
+++ generate_law_pdf.py
@@ -5,12 +5,13 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 import re
+import os
 
 # Hugging Face에서 데이터셋 불러오기
 dataset = load_dataset("joonhok-exo-ai/korean_law_open_data_precedents", download_mode="force_redownload")
 
-# 처음 10개의 항목만 변환 (테스트를 위해 10개로 제한)
-sample_data = dataset['train'].select(range(10))  # 10개 선택
+# 처음 5000개의 항목만 변환 (테스트를 위해 5000개로 제한)
+sample_data = dataset['train'].select(range(10000))
 
 def convert_to_detailed_structure(full_text):
     # 【】 안의 내용을 키로 하고, 나머지 텍스트를 값으로 하는 딕셔너리 생성
@@ -26,9 +27,9 @@ def convert_to_detailed_structure(full_text):
     
     return section_dict
 
-def create_pdf(case_data, font_path):
+def create_pdf(case_data, font_path, output_dir):
     # PDF 파일 이름
-    pdf_file = f"{case_data['판례정보일련번호']}.pdf"
+    pdf_file = os.path.join(output_dir, f"{case_data['판례정보일련번호']}.pdf")
     
     # PDF 문서 설정
     doc = SimpleDocTemplate(pdf_file, pagesize=A4, rightMargin=20, leftMargin=20, topMargin=20, bottomMargin=20)
@@ -47,13 +48,14 @@ def create_pdf(case_data, font_path):
 
     # 나머지 데이터 추가
     for key, value in case_data.items():
-        if key == "참조판례":  # 참조판례는 제외
-            continue
-        if key == "전문":  # 전문 내용을 상세 구분으로 나누기
+        if key == "참조판례":  # 참조판례가 존재하는 경우에만 추가
+            if value:
+                elements.append(Paragraph(f"{key}: {', '.join(value)}", normal_style))
+                elements.append(Spacer(1, 12))
+        elif key == "전문":  # 전문 내용을 상세 구분으로 나누기
             detailed_sections = convert_to_detailed_structure(value)
             for section, text in detailed_sections.items():
-                elements.append(Paragraph(f"{section}:", heading_style))
-                elements.append(Paragraph(text, normal_style))
+                elements.append(Paragraph(f"{section}: {text}", normal_style))
                 elements.append(Spacer(1, 12))
         else:
             if isinstance(value, list):  # 리스트 타입 처리
@@ -69,8 +71,13 @@ def create_pdf(case_data, font_path):
 
 # 폰트 파일 경로 설정
 font_path = "C:\\Users\\OWNER\\inhye\\Natural_language\\인혜,민정_도전학기\\Noto_Sans_KR\\NotoSansKR-VariableFont_wght.ttf"
+# 저장 디렉토리 설정
+output_dir = "D:\\pdfs"
+
+# 저장 디렉토리 존재 확인 및 생성
+os.makedirs(output_dir, exist_ok=True)
 
 # 각 판례 데이터를 PDF로 변환
 for case in sample_data:
-    pdf_file = create_pdf(case, font_path)
+    pdf_file = create_pdf(case, font_path, output_dir)
     print(f"Created PDF: {pdf_file}")
